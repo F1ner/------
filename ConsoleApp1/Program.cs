ab201026842a96c85f36913155fd2baf3c730d0e
F1ner <123871318+F1ner@users.noreply.github.com>
2023-02-20 11:18:14 +0200

    а

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("n=");
+            //byte n = byte.Parse(Console.ReadLine());
+            //int[] array = new int[];
+            //for(int i=0;i<array.Length;i++)
+            //{
+            //    Console.Write($"array[{i}]=");
+            //    array[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //for(int i=0;i<array.Length;i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //Console.Write("n=");
+            //byte n = byte.Parse(Console.ReadLine());
+            //int[] array = new int[];
+
+
+
+
+            //Random rnd = new Random();
+            //for(int i=0;i<array.Length;i++)
+            //{
+            //    array[i] = rnd.Next(-20, 20);
+
+            //}
+            //foreach(int elem in array)
+            //{
+            //    Console.Write($"{elem}\t");
+            //}
+
+
+        }
+    }
+}
